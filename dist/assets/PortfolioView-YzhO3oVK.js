da73a06b415d572fb038cf19860fbe6b2b812fd6
Arlekin <frothen-trone@mail.ru>
2024-05-25 15:14:28 +0500

    dist

--- dist/assets/PortfolioView-YzhO3oVK.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as e,o as c}from"./index-DWGpjOsi.js";const t={},r={class:"Portfolio"};function s(n,_){return c(),e("div",r)}const f=o(t,[["render",s]]);export{f as default};
---
+++ dist/assets/PortfolioView-biv4AGev.js
@@ -0,0 +1,1 @@
+import{_ as o,c as e,o as c}from"./index-Kij6hgdR.js";const t={},r={class:"Portfolio"};function s(n,_){return c(),e("div",r)}const f=o(t,[["render",s]]);export{f as default};
